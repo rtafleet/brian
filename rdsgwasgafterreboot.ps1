e83792647d0c7be93d4229661469b45aa407cb1e
bgollmer <bgollmer@rtafleet.com>
2018-05-14 23:24:51 -0700

    source folder notation

--- rdsgwasgafterreboot.ps1
+++ rdsgwasgafterreboot.ps1
@@ -1,3 +1,5 @@
+#### File needs saved to \\rta-fs\tempwebsitedownloads\Brian folder whenever it's modified
+
 New-Item -Path "C:\" -ItemType Directory -Name "temp"
 New-Item -Path "C:\windows\temp" -Name "testuserdata.txt" -ItemType "file" -Value "Yep, userdata obviously ran"
 Add-RDServer -Server "$env:computername.$env:userdnsdomain" -Role "RDS-GATEWAY" -ConnectionBroker "aws-rdscb01.rta-host.com"
@@ -11,3 +13,27 @@ cd RDS:\GatewayServer\RAP\Default-RAP\UserGroups
 New-Item -Name "domain users@RTA-HOST"
 cd RDS:\GatewayServer
 Set-Item -Path SSLBridging -Value 2
+
+
+#replace rdsweb pages with branded stuff
+
+
+
+#Import-Module ActiveDirectory
+#$Computer = Get-Content env:computername
+#If (Get-ADObject $Computer -eq $null) 
+#{
+#$User = Read-Host -Prompt "Enter user with domain joining privileges"
+#$Password = Read-Host -Prompt "Enter password for $user" -AsSecureString 
+#$Domain = Read-Host -Promt "Enter the domain" 
+#$Username = "$Domain\$User" 
+#$Credential = New-Object System.Management.Automation.PSCredential($Username,$Password) 
+#Add-Computer -DomainName $domain -Credential $Credential
+#}
+#else {
+#Write-Host "$Computer already exists in the domain"
+#}
+
+
+
+####cross zone load balancing??????
